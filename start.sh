10d2d0ed4af3cc4fa6bb50e4e50fef39823fa4c6
Glocke <cgloeckner@freenet.de>
2022-03-25 08:47:26 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 sleep 3s
 firefox --new-window http://localhost:8080
 sleep 3s
-xdotool search --sync --onlyvisible -class "firefox-esr" key F11
+xdotool search --sync --onlyvisible --class "firefox-esr" key F11
