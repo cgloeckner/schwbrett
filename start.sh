ab437c3a613dc1af42f9fa344f62fd44bec530d1
Glocke <cgloeckner@freenet.de>
2022-06-23 09:47:02 +0200

    include auto-update into startup

--- start.sh
+++ start.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# try to pull update
+git pull
+
+# run server
 ./main.py &
 sleep 3s
+
+# start browser
 firefox --new-window http://localhost:8080 &
 sleep 3s
+
+# trigger fullscreen
 xdotool search --sync --onlyvisible --class "firefox-esr" key F11
